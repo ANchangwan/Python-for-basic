3f207b29b4c8a226b8ec0b8265387e40b7e4e928
ANchangwan <56478474+ANchangwan@users.noreply.github.com>
2020-07-01 12:41:25 +0900

    Add files via upload

---
+++ section02-01.py
@@ -0,0 +1,24 @@
+# Section02 - 1
+# 파이썬 기초 코딩
+# Print  구문의 이해
+
+#기본 출력
+
+print('Hello Python!')
+print("Hello Python!")
+print("""Hello Python!""")
+print('''Hello Python!''')
+
+print() #프린트 함수는 안에 아무것도 없을 때 프린트 함수라고 한다.
+
+# Separator 옵션 사용
+print('T','E','S','T',sep='')
+print('2019','02','19',sep='-') # sep 문자 사이에 공백의 내용을 삽입해준다.A
+print('niceman','google.com',sep=' @ ')
+
+#end 옵션 사용
+print('Welcome To', end=' ')
+print('the black parade', end=' ') # end는 끝부분을 다음 문장과 연결해준다.
+print(' piano notes')
+
+print("Test")
