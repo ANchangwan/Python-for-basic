54534dbd50f10beaf1b4754d287491edb01f5c32
ANchangwan <56478474+ANchangwan@users.noreply.github.com>
2020-07-19 17:14:35 +0900

    Update section05-q.py
    1. list comprehension
    
    변수 = [(변수값)x for x in range(시작값, 끝값) if 조건문]

--- section05-q.py
+++ section05-q.py
@@ -7,7 +7,11 @@
 q1 =  {"봄": "딸기", "여름": "토마토", "가을": "사과"}
 for v in q1.keys():
     if v =='가을':
-        print("q1 : ", v)
+        print("q1 : ",q1[v])
+
+for v,s in q1.items():
+    if v == '가을':
+        print("q1 : ",s)
 
 
 print()
@@ -18,6 +22,9 @@ q2 =  {"봄": "딸기", "여름": "토마토", "가을": "사과"}
 for v in q2.values():
     if v == "사과":
         print("q2 : %s가 포함돼어있습니다." %v)
+        break # 사과 있으면 else문이 수행이 안되고 없으면 수행됨
+else:
+    print("사과 없음")
 
 
 
@@ -29,20 +36,18 @@ for v in q2.values():
 # 21 ~ 40 :  D학점
 #  0 ~ 20 :  E학점
 
-#q3 = {'A' : 82, 'B' : 76, 'C': 55,'D':30, 'E' : 19}
-#for v in q3.velus():
-    #if v == q3.range(81,100):
-     #   print("a학점입니다.")
-    #elif v == q3.range(61,80):
-     #   print('b학점입니다.')
-    #elif v == q3.range(41,60):
-     #   print('c학점입니다.')
-    #elif v == q3.range(21,40):
-     #   print('D학점입니다.')
-    #elif v == q3.range(0,20):
-     #   print("E학점입니다.")
+q3 = 77
 
-
+if q3 >=81:
+    print("A학점")
+elif q3 >=61:
+    print("B학점")
+elif q3 >=41:
+    print("C학점")
+elif q3 >=21:
+    print("D학점")
+elif q3 >= 0 :
+    print("E학점")        
 
 
 # 4. 다음 세 개의 숫자 중 가장 큰수를 출력하세요.(if문 사용) : 12, 6, 18
@@ -57,7 +62,11 @@ print("가장 큰수 : ", max)
 
 
 # 5. 다음 주민등록 번호에서 7자리 숫자를 사용해서 남자, 여자를 판별하세요. (1,3 : 남자, 2,4 : 여자)
-
+s = '891022-2473837'
+if int(s[7]) % 2 == 1:
+    print("남자")
+else:
+    print("여자")    
 
 # 6 ~ 10 반복문 사용(while 또는 for)
 
@@ -72,8 +81,12 @@ q3 = ["갑", "을", "병", "정"]
 
 for v in q3:    
     if v!='정':
-        print("%s" %v)
+        print("%s" %v,end='')
     
+print()
+
+q5 = [x for x in q3 if x !='정']
+print(q5)
 
 # 7. 1부터 100까지 자연수 중 '홀수'만 한 라인으로 출력 하세요.
 
@@ -81,8 +94,11 @@ q7 = 0
 while q7<100:
     q7+=1
     if q7%2 != 0:
-        print(q7)
-
+        print(q7,end=',')
+print()
+print('-'*50)
+q5 = [x for x in range(1,101) if x%2 != 0]
+print(q5)
 
 
 # 8. 아래 리스트 항목 중에서 5글자 이상의 단어만 출력하세요.
@@ -97,7 +113,7 @@ for i in q4:
 q5 = ["A", "b", "c", "D", "e", "F", "G", "h"]
 
 for i in q5:
-    if i.isupper():
+    if i.islower():
         print(i)
 
 
@@ -111,3 +127,20 @@ for i in q6:
 for i in q6: 
     if i.islower() == True:
         print("대문자 : ", i.upper())
+
+
+# 리스트 컴프리헨션
+
+
+# 일반적이 방법
+numbers = []
+
+for n in range(1, 101):
+    numbers.append(n)
+
+print(numbers)
+
+print()
+
+numbers2 = [x for x in range(1,101)]
+print(numbers2)
