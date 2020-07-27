dc770069d1c17b2f91fdcdc7f5f6d4857a192b1c
ANchangwan <56478474+ANchangwan@users.noreply.github.com>
2020-07-27 22:11:48 +0900

    파일 읽기, 쓰기

---
+++ section09.py
@@ -0,0 +1,54 @@
+# Section09
+# 파일 읽기, 쓰기
+# 읽기 모드 : r , 쓰기 모드(기존 파일 삭제) : w, 추가모드(파일 생성 또는 추가) : a
+#.. : 상대경로, . : 절대 경로
+
+# 파일 읽기
+# 예제1
+
+f = open('./resource/review.txt', 'r')
+content = f.read()
+print(content)
+print(dir(f))
+# 반드시 close 리소스 반환
+f.close()
+
+
+print("--------------------------------------")
+
+# 예제2
+with open('./resource/review.txt', 'r') as f:
+    # with 문은 close를 안해도 자동으로 반환해줌
+    c = f.read()
+    print(c)
+    print(list(c))
+    print(iter(c))
+
+
+print("----------------------------------")
+print("----------------------------------")
+
+# 예제3
+with open('./resource/review.txt', 'r') as f:
+    for c in f:
+        print(c.strip())
+
+
+print("----------------------------------")
+print("----------------------------------")
+
+# 예제4
+with open('./resource/review.txt', 'r') as f:
+    content = f.read()
+    print(">",content)
+    content = f.read() # 내용 없음
+    print(">",content)
+
+# 예제5
+with open('./resource/review.txt', 'r') as f:
+    line = f.readline()
+    #print(line)
+    while line:
+        print(line, end='###')
+        line = f.readline()
+
