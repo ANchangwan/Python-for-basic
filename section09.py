c1d155570c9451689d10ea3c90e999a2f133130c
ANchangwan <56478474+ANchangwan@users.noreply.github.com>
2020-07-29 23:01:53 +0900

    파일 일기, 쓰기

--- section09.py
+++ section09.py
@@ -52,3 +52,51 @@ with open('./resource/review.txt', 'r') as f:
         print(line, end='###')
         line = f.readline()
 
+# 예제6
+with open('./resource/review.txt', 'r') as f:
+    contents = f.readlines()
+    print(contents)
+    for c in contents:
+        print(c, end=' ****** ')
+
+print("---------------------")
+print("---------------------")
+
+print()
+# 예제7
+
+with open('./resource/score.txt','r') as f:
+    score = []
+    for line in f:
+        score.append(int(line))
+    print(score)
+
+print('Average : {:6.3}'.format(sum(score)/len(score)))
+
+
+# 파일 쓰기
+# 예제1
+with open('./resource/text1.txt','w') as f:
+    f.write('NiceMan!')
+
+# 예제2
+with open('./resource/text1.txt','w') as f:
+    f.write('GoodMan!\n')
+
+# 예제3
+from random import randint
+with open('./resource/text2.txt','w') as f:
+ for cnt in randge(6):
+     f.write(str(randint(1,50)))
+     f.write('\n')
+
+# 예제4
+# writelines : 리스트 -> 파일로 저장
+with open('./resource/text3.txt','w') as f:
+    list = ['kim\n','park\n','cho\n']
+    f.writelines(list)
+
+# 예제5
+with open('./resource/text4.txt','w') as f:
+    print('Test contests1!', file=f)
+    print('Test contests2!', file=f)
