aedb7e6f2a88f65d7e7332f5c42330b683a81af6
ANchangwan <56478474+ANchangwan@users.noreply.github.com>
2020-09-05 23:02:14 +0900

    Add files via upload

--- Advanced_python/chapter01_02.py
+++ Advanced_python/chapter01_02.py
@@ -6,7 +6,7 @@
 
 # 클래스 재 선언
 
-class Student():
+class Student(): # 모든 클래스의 부모는 object
     """
     Student class
     Author : An
@@ -79,6 +79,63 @@ print()
 student1.details_info()
 student2.details_info()
 
+# 에러
+# Student.details_info()
+
+Student.details_info(student1)
+print()
+print()
+# 클래스 이름으로 접근할때는 인스턴스화된 변수를 넣어서 사용하면 직접 접근이 된다.
+
+# 비교
+print(student1.__class__,student2.__class__) # 부모 클래스를 알려줌
+print(id(student1.__class__) == id(student2.__class__))
+
+print()
+
+# 인스턴스 변수
+# 직접 접근(PEP 직접 접근을 권장 x)
+# 데이터는 캡슐화를 해서 은닉해야 됨
+
+student1.name = 'changeName'
+print(student1.name)
+
+print()
+print()
+
+
+# 클래스 변수
+
+# 접근
+#---> 접근은 자유롭게 할 수 있다. 왜냐하면 클래스 변수는 공용이기 때문에 가능
+# 단, 인스턴스 변수는 인스턴스변수로 접근해야된다.
+print(student1.student_count)
+print(student2.student_count)
+print(Student.student_count)
+
+
+print()
+print()
+
+# 공유 확인
+print(Student.__dict__)
+print(student1.__dict__) # 클래스 변수가 출력되지 않음
+print(student2.__dict__)
+
+print()
+print()
+
+# (중요)
+# 인스턴스 네임스페이스 없으면 상위에서 검색(파이썬이 알아서 확인)
+# 즉, 동일한 이름을 변수 생성 간으(인스턴스 검색 후 -> 상위(클래스 변수, 부모 클래스 변수))
+
+
+del student2
+
+print(student1.student_count)
+print(Student.student_count)
+
+print(Student.name) # 역은 성립하지 않음. 클래스에 없는데 인스턴스 변수에는 존재하더라도 출력 되지 않는다.
 
 # 클래스는 속성과 메소드로 구성
 # 속성 : 인스턴스변수, 클래스 변수
