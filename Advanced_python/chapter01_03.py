ad63774eb5f54593ef3ca538c90bf0bac02605ab
ANchangwan <56478474+ANchangwan@users.noreply.github.com>
2020-09-08 23:27:56 +0900

    메소드 상세 설명(클래스 메소드, 인스턴스 메소드, 스테틱 메소드)
    1. Class Method : 
    - 의미전달을 분명하게 할 수 있음. 
    - class로 인자를 받음
    2. Instance Method : 
    - self로 인자를 받음
    3. Static Method : 
    - 인스턴스, 클래스를 공용으로 사용 가능. 둘 다 받음
    4. 효율적인 클래스 상세 설명

--- Advanced_python/chapter01_03.py
+++ Advanced_python/chapter01_03.py
@@ -2,6 +2,7 @@
 # 파이썬 심화
 # 클래스 메소드, 인스턴스 메소드, 스테이틱 메소드(class method, instance method, static method)
 # 인스턴스 메소드 -> self로 인자를 받음
+# 클래스 메소드 -> 클래스변수를 인자로 받음
 
 
 # 기본 인스턴스 메소드
@@ -39,11 +40,16 @@ class Student(object):
     # Instance Method
     def get_fee_culc(self):
         return 'After Tuition -> id: {}, fee : {}'.format(self.id,self.tuition * Student.tuition_per)
+
     
     def __str__(self):
         return 'Student Info ->name : {} grade: {} email: {}'.format(self.full_name(),self.grade,self.email)
     
     # Class Method
+    # Class Method가 받는 인자는 클래스 자체를 받음
+    # 클래스변수에도 접근가능, 클래스를 생성할 수 있는 인스턴스할 수있고 로직을 만들 수 도 있다.
+    # 권장방식 : 의미전달을 확실하게 해줄 수 있기 때문
+    # cls = class Student
     @classmethod
     def raise_fee(cls, per):
         if per < 1:
@@ -51,6 +57,20 @@ class Student(object):
             return 
         cls.tuition_per = per  # cls = Student 클래스 인자가 넘어옴, Class과 같음 
         print('Succed! tuition increased!') 
+
+    # Class Method
+    @classmethod
+    def student_cont(cls,id, first_name, last_name, email, grade, tuition, gpa):
+        return cls(id, first_name, last_name, email, grade, tuition * cls.tuition_per, gpa)
+
+    # Static Method
+    # 공통적으로 사용하고 싶을 때 사용
+    # static으로 할 수 있는건 인스턴스로 할 수 있고 클래스로도 사용할 수 있음
+    @staticmethod
+    def is_scholarship_st(inst):
+        if inst.gpa >= 4.3:
+            return '{} is a scholarship recipient.'.format(inst.last_name)
+        return 'Sorry. Not a scholarship recipient'    
 
 
 # 학생 인스턴스
@@ -83,3 +103,51 @@ Student.raise_fee(1.5)
 # 학비 정보(인상 후)
 print(student_1.get_fee_culc())
 print(student_2.get_fee_culc())
+
+# 클래스 메소드 인스턴스 생성 실습
+# 의미 전달이 확실해서 권장
+student_3 = Student.student_cont(3, 'Park','Minji','Student3@gmail.com','3', 550, 4.5)
+student_4 = Student.student_cont(4, 'Cho','Sunghan','Student4@gmail.com','4', 600, 4.1)
+
+# 전체 정보
+
+print(student_3.detail_info())
+print(student_4.detail_info())
+print()
+
+# 학생 학비 변경 확인
+print(student_3.tuition)
+print(student_4.tuition)
+print()
+
+# 장학금 혜택 여부(스테이틱 메소드 미사용)
+# inst => 인스턴스 변수 삽입
+def is_scholarship(inst):
+    if inst.gpa >= 4.3:
+        return '{} is a scholarship recipient.'.format(inst.last_name)
+    return 'Sorry. Not a scholarship recipient'
+
+
+# 함수로 접근
+print(is_scholarship(student_1))
+print(is_scholarship(student_2))
+print(is_scholarship(student_3))
+print(is_scholarship(student_4))
+
+print()
+
+# 장학금 혜택 여부(스테이틱 메소드 사용)
+# 클래스로 접근
+print(Student.is_scholarship_st(student_1))
+print(Student.is_scholarship_st(student_2))
+print(Student.is_scholarship_st(student_3))
+print(Student.is_scholarship_st(student_4))
+
+print()
+
+# 인스턴스 변수로도 사용가능
+# 인스턴스로 접근
+print(student_1.is_scholarship_st(student_1))
+print(student_1.is_scholarship_st(student_2))
+print(student_1.is_scholarship_st(student_3))
+print(student_1.is_scholarship_st(student_4))
