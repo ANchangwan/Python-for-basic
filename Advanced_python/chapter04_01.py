748855e9233e2660be560da2f9f44e965864db1f
ANchangwan <56478474+ANchangwan@users.noreply.github.com>
2020-10-21 22:32:35 +0900

    일급 함수 설명

--- Advanced_python/chapter04_01.py
+++ Advanced_python/chapter04_01.py
@@ -75,5 +75,88 @@ print()
 
 # Callable : 호출 연산자 -> 메소드 형태로 호출 가능한지 확인
 
+import random
+
+# 로또 추첨 클래스 선언
+class LottoGame:
+    def __init__(self) -> None:
+        self.balls = [int(n) for n in range(1,45)]
+
+    def pick(self):
+        random.shuffle(self.balls)
+        return sorted([random.choice(self.balls) for n in range(6)])
+    
+    def __call__(self): # 호출 가능하게 하는 맥직 메소드(함수처럼 호출) *** 중요
+        return self.pick()
+
+# 객체 생성
+game = LottoGame()
+
+# 게임 실행
+# callable : 호출 가능 확인
+
+print('EX4-1 -', callable(str),callable(list),callable(factorial), callable(3.14), callable(game)) # 3.14는 함수가 아니기 때문에 False가 뜸
+print('EX4-2 -', game.pick())
+print('EX4-3 -',game())
+print('EX4-4 -', callable(game))
+
+# ** 클래스를 매직 메소드를 이용해서 변수,함수, 클래스로도 사용 가능한다.
+
+print()
+print()
+
+# 다양한 매개변수 입력(*args, **kwargs)
+def args_test(name, *contents, point =None, **attrs):
+    return '<agrs_test> -> ({}) ({}) ({}) ({})'.format(name,contents, point, attrs)
+
+print('EX5-1 -',args_test('test1')) # 패킹은 튜플로 받음
+print('EX5-2 -',args_test('test1', 'test2'))
+print('EX5-3 -',args_test('test1', 'test2','test3',id='admin',point = 7)) # point랑 이름이 같은 값에 대입 만약에 없으면 계속 None으로 받음
+print('EX5-4 -',args_test('test1', 'test2','test3',id='admin',point = 7, password ='1234'))
+
+print()
+print()
+
+# 함수 Signatures
+from inspect import signature # 함수에 인자에 정보를 표시 클래스형태에 메소드
+
+sg = signature(args_test)
+
+print('EX5-2 -', sg)                # 파라미터를 보여줌
+print('EX6-2 -', sg.parameters)     # 파라미터를 디테일하게 보여줌
+
+print()
+print()
+
+# 모든 정보 출력
+for name, param in sg.parameters.items():
+    print('EX6-3 -', name, param.kind, param.default)
 
 
+print()
+print()
+
+# partial 사용법 : 인수 고정 -> 주로 특정 인수 고정 후 콜백 함수에 사용
+# 하나 이상의 인수가 이미 할당된(채워진) 함수의 새 버전 반환
+# 함수의 새 객체 타입은 이전 함수의 자체를 기술하고 있다.
+
+
+
+
+from operator import mul
+from functools import partial
+
+print('EX7-1 -', mul(10,1000))
+
+
+# 인수 고정
+
+five = partial(mul, 5)
+
+# 고정 추가
+six = partial(five, 6)
+
+print('EX7-2 -',five(1000))
+print('EX7-3 -', six())
+print('EX7-4 -', [five(i) for i in range(1,11)])
+print('EX7-5 -', list(map(five,range(1,11))))
