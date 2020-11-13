6a7555694b779d59831f96c1f580254731c243df
ANchangwan <56478474+ANchangwan@users.noreply.github.com>
2020-11-13 17:34:22 +0900

    파이썬 클래스 관련 메소드 심화
    o private 속성 
    - @property 데코라이터 사용
    
    o __slot__ 
    - 메모리 절감, 많이 복사되는 객체는 __slot__옵션 사용
    
    o 객체 슬라이싱, 인덱싱
    - 객체 전체를 자동으로 조사
    
    o ABC, 상속,  오버라이딩

--- Advanced_python/chapter05_02.py
+++ Advanced_python/chapter05_02.py
@@ -27,7 +27,7 @@ class VectorP(object):
         print('Called Property Y')
         return self.__y
 
-    @x.setter 
+    @y.setter 
     def y(self,v):
         if v < 30:
             raise ValueError('30 아래는 사용할 수 없습니다.')
@@ -49,15 +49,203 @@ v = VectorP(20, 40)
 
 # Getter : 삽입 
 # Setter : 수정
+print('EX1-2 -', dir(v), v.__dict__)
+print('EX1-3 -', v.x, v.y)
 
-print(v.x)
 
 
-v.y = 20
 
 
 
 
 # Iter 확인
 for val in v:
-    print("EX1-2 -", val)
+    print("EX1-4 -", val)
+
+
+# __slot__
+# 파이썬 인터프리터에게 통보
+# 해당 클래스가 가지는 속성을 제한
+# __dict__ 속성 최적화 -> 다수 객체 생성시 -> 메모리 사용 공간 대폭 감소
+# 해당 클래스에 만들어진 인스턴스 속성 관리에 딕션너리 대신 Set 형태를 사용
+# __slot__이 더 빠르다
+
+
+class TestA(object):
+    __slots__ = ('a',)
+
+class TestB(object):
+    pass
+
+use_slot = TestA()
+no_slot = TestB()
+
+print('EX2-1 -', use_slot)              
+#print('EX2-2 -', use_slot.__dict__)    # dic 대신 set를 사용하기 때문에 에러 발생
+print('EX2-3 -', no_slot)
+print('EX2-4 -', no_slot.__dict__)
+
+# 메모리 사용량 비교
+from random import Random
+import timeit
+
+# 측정을 위한 함수 선언
+
+def repeat_outer(obj):
+    def repeat_inner():
+        obj.a = 'TEST'
+        del obj.a
+    return repeat_inner
+
+
+print(min(timeit.repeat(repeat_outer(use_slot), number =1000)))
+print(min(timeit.repeat(repeat_outer(no_slot), number =1000)))
+
+print()
+print()
+
+# 객체 슬라이싱
+
+class ObjecS:
+    def __init__(self):
+        self._numbers = [n for n in range(1, 10000, 3)]
+    
+    def __len__(self):
+        return len(self._numbers)
+    
+    def __getitem__(self, idx):
+        return self._numbers[idx]
+
+s = ObjecS()
+
+print('EX3-1 -', s.__dict__)
+print('EX3-2 -', len(s))
+print('EX3-3 -', len(s._numbers))
+print('EX3-4 -',s[1:100])
+print('EX3-5 -', s[-1])
+print('EX3-6 -', s[::10])
+
+
+print()
+print()
+
+# 파이썬 추상클래스
+# 참고 : http: // docs.python.org/3/library/collections.abc.html
+
+# 자체적으로 객체 생성 불가
+# 상속을 통해서 자식 클래스에서 인스턴스 생성해야 함
+# 개발과 관련된 공통된 내용(필드, 메소드) 추출 및 통합해서 공통된 내용으로 작성하게 하는 것
+
+
+# Sequence 상속 받지 않았지만, 자동으로 __iter__, __contain__  기능 작동
+# 객체 전체를 자동으로 조사 -> 시퀀스 프로토콜
+
+
+class IterTestA():
+    def __getitem__(self, idx):
+        return range(1, 50, 2)[idx] # range(1, 50, 2)
+
+i1 = IterTestA()
+
+print('EX4-1 -', i1[4])
+print('EX4-2 -', i1[4:10])
+print('EX4-3 -', 3 in i1[1:10])
+# print('EX4-4 -',[i for i in i1])
+
+print()
+print()
+
+# Sequence 상속
+# 요구사항인 추상메소드를 모두 구현해야 동작
+
+from collections.abc import Sequence
+
+class IterTestB(Sequence): # 부모 : Sequence
+    def __getitem__(self, idx):
+        return range(1, 50, 2)[idx] # range(1, 50, 2)
+    
+    def  __len__(self, idx):
+        return len(range(1, 50, 2)[idx])
+
+
+i2 = IterTestB()
+print('EX4-5 -', i2[4])
+print('EX4-6 -', i2[4:10])
+print('EX4-7 -', 3 in i2[1:10])
+
+
+print()
+print()
+
+
+
+# abc 활용 예제
+
+import abc
+
+class RandomMachine(abc.ABC): # metaclass=abc.ABCMeta(3.4 이하)
+    # __metaclass__ = abc.ABCMeta
+
+    # 추상 메소드
+    @abc.abstractclassmethod
+    def load(self, iterobj):
+        '''Iterable 항목 추가'''
+
+
+    # 추상 메소드
+    @abc.abstractclassmethod
+    def pick(self, iterobj):
+        '''무작위 항목 뽑기'''
+    
+    def inspect(self):
+        items = []
+        while True:
+            try:
+                items.append(self.pick())
+            except LookupError:
+                break
+            return tuple(sorted(items))
+
+
+import random
+
+class CraneMachine(RandomMachine):
+    def __init__(self, items) -> None:
+        self._randomizer = random.SystemRandom()
+        self._items = []
+        self.load(items)
+
+    def load(self, items):
+        self._items.extend(items)
+        self._randomizer.shuffle(self._items)
+    
+    def pick(self):
+        try:
+            return self._items.pop()
+        except IndexError:
+            raise LookupError('Empty crane Box')
+    
+    def __call__(self):
+        return self.pick()
+
+
+# 서브 클래스 확인
+print('EX5-1 -', issubclass(RandomMachine, CraneMachine))
+print('EX5-2 -', issubclass(CraneMachine, RandomMachine)) # issubclass(자식, 부모) 
+
+
+# 상속 구조 확인
+print('EX5-3 -', CraneMachine.__mro__)
+
+cm = CraneMachine(range(1, 100)) # 부모클래스에 추상 메소드는 반듯이 자식클래스에 구현해야 한다.
+                                 # 추상 메소드는 구현 안하면 에러
+
+print('EX5-4 -', cm._items)
+print('EX5-5 -', cm.pick())
+print('EX5-6 -',cm())
+print('EX5-6 -', cm.inspect()) # 자식메소드에 없지만 부모클래스에 있으면 실행가능하다.
+
+
+# Private 속성 -> @property  데코라이터 이용해서 은닉 가능 직접접근해서 수정을 막을 수 있음
+#__slot__
+# 객체슬라이싱, 인덱싱 :
